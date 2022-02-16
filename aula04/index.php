b8a4170bd3a99c3e3a8c8a802edf3afbf3e1fe4e
JonasPinto <jonaspinto.ti@outlook.com>
2022-02-16 17:18:18 -0300

    Create index.php

---
+++ aula04/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $n = 4;
+        $no = "Jonas";
+        echo $no;
+        ?>
+    </body>
+</html>
