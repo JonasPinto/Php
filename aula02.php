900ac96daf80eae6302749618787a8d64307478b
JonasPinto <jonaspinto.ti@outlook.com>
2022-02-16 18:06:14 -0300

    Update aula02.php

--- aula02.php
+++ aula02.php
@@ -1,7 +1,32 @@
-<?php
-
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
-
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Primeiro exemplo PHP</title>
+        <style>
+            h2 {
+                color:#80a2ff;
+                    text-shadow: 1px 1px 1px black;
+            }
+            h3 {
+                tex
+                text-align: center;
+                color:#cc0000;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Testando PHP</h1>
+        <?php
+        $idade = 3;
+        if ($idade > 0);
+            echo "<h3>tudo certo com a idade</h3>";
+        
+        echo "<h2>Olá,<br/> Mundo!</h2>";
+        ?>
+    </body>
+</html>
