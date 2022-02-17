2ef3c21b1bc745f4e48c4f931424a39a50d7cd07
JonasPinto <jonaspinto.ti@outlook.com>
2022-02-17 16:25:20 -0300

    Update ex03.php

--- exercicios/ex03.php
+++ exercicios/ex03.php
@@ -1,14 +1,19 @@
 <!doctype html>
 <html>
     <head>
-        <title>exercicio_02</title>
+        <title>Exercício_03</title>
         <link rel="stylesheet" href="../_css/estilo_ex.css"/>
     </head>
     <body>
-<?php
-/*Exercício - 3  A Loja Mamão com Açúcar está vendendo seus produtos em 5 (cinco) prestações sem juros.
-Faça um algoritmo que receba um valor de uma compra e mostre o valor das prestações*/
+        <?php
+        /*Exercício - 3  http://localhost/PHP/PHP/exercicios/ex03.php?prc=16.99
+        A Loja Mamão com Açúcar está vendendo seus produtos em 5 (cinco) 
+        prestações sem juros.
+        Faça um algoritmo que receba um valor de uma compra e mostre o valor das prestações*/
+        
+        $prc = $_GET["prc"];
+        echo "<h2> Sua compra será parcelada em 5 x de ". number_format($prc / 5,2) ." sem juros <h2/>";
 
-?>
+        ?>
     </body>
 </html>
