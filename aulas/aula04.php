f7ec56addf9544cd0744644de371715c09a43d61
JonasPinto <jonaspinto.ti@outlook.com>
2022-02-17 09:29:58 -0300

    Create aula04.php

---
+++ aulas/aula04.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Aula 04</title>
+        <link rel="stylesheet" href="../_css/estilo.css"/>
+    </head>
+    
+    <body>
+    <div>
+        <?php /*http://localhost/PHP/PHP/aulas/aula04.php?idade=34*/
+        $idade = $_GET["idade"];
+        $nome = "Jonas";
+        echo " $nome tem $idade anos ";      
+        ?>
+    <div/>    
+    </body>
+</html>
