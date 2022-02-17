b8c01ab85fce1322fa1c4c441401a6b068dec5e9
JonasPinto <jonaspinto.ti@outlook.com>
2022-02-17 09:29:33 -0300

    Update ex01.php

--- exercicios/ex01.php
+++ exercicios/ex01.php
@@ -1,11 +1,12 @@
 <!doctype html>
 <html>
     <head>
-        <title>Exercicio _01</title>
+        <title>Exercicio_01</title>
+        <link rel="stylesheet" href="../_css/estilo_ex.css"/>
     </head>
     <body>
     <?php
-    /*Exercício - 1 
+    /*Exercício - 1  http://localhost/PHP/PHP/exercicios/ex01.php?qtd=50&ct=5.45
         Faça o algoritmo que calcule o valor em Reais,
      *  correspondente aos dólares que um turista possui no cofre do hotel. O programa deve
         solicitar os seguintes dados: Quantidade de
@@ -14,7 +15,7 @@
     $qtd = $_GET["qtd"];
     $ct = $_GET["ct"];
     $calc = $qtd * $ct;
-    echo " $ ".$qtd." dolares convertidos em reais são ".$calc;
+    echo "<h2> US$$qtd dolares convertidos em reais são $$calc <h2/>";
     
     ?>
     </body>
