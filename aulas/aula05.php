703a4d60b186915af7a80a7effff1072c888eb8f
JonasPinto <jonaspinto.ti@outlook.com>
2022-02-17 09:29:36 -0300

    Create aula05.php

---
+++ aulas/aula05.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Aula 05</title>
+        <link rel="stylesheet" href="../_css/estilo.css"/>
+    </head>
+    <body>
+<?php
+
+?>
+    </body>
+</html>
+
