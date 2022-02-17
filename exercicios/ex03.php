27ac4e050345b424070cfa51a6d8db60933868d5
JonasPinto <jonaspinto.ti@outlook.com>
2022-02-17 09:29:11 -0300

    Create ex03.php

---
+++ exercicios/ex03.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+    <head>
+        <title>exercicio_02</title>
+        <link rel="stylesheet" href="../_css/estilo_ex.css"/>
+    </head>
+    <body>
+<?php
+/*Exercício - 3  A Loja Mamão com Açúcar está vendendo seus produtos em 5 (cinco) prestações sem juros.
+Faça um algoritmo que receba um valor de uma compra e mostre o valor das prestações*/
+
+?>
+    </body>
+</html>
