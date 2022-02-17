5ca2e540e983360ccea2dde4b9f99a144f37d5bf
JonasPinto <jonaspinto.ti@outlook.com>
2022-02-17 16:25:29 -0300

    Update ex02.php

--- exercicios/ex02.php
+++ exercicios/ex02.php
@@ -1,13 +1,13 @@
 <!doctype html>
 <html>
     <head>
-        <title>exercicio_02</title>
+        <title>Exercício_02</title>
         <link rel="stylesheet" href="../_css/estilo_ex.css"/>
     </head>
  
     <body>
     <?php
- /*   Exercicio - 2 url -> http://localhost/PHP/PHP/exercicios/ex02.php?dist=1500&vl=20
+ /*   Exercício - 2 url -> http://localhost/PHP/PHP/exercicios/ex02.php?dist=1500&vl=20
       Faça um algoritmo que após a entrada de uma determinada distância entre dois pontos(Km),
   *     e uma determinada velocidade(Km/h), diga qual o tempo médio que levará para chegada à esse 
   *     local e qual a velocidade em metros/segundos*/
