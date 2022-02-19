f183d46a469789a8457ec123e5fcc3d8bfbeb8e8
JonasPinto <jonaspinto.ti@outlook.com>
2022-02-19 01:49:41 -0300

    Update ex01.php

--- exercicios/ex01.php
+++ exercicios/ex01.php
@@ -6,7 +6,7 @@
     </head>
     <body>
     <?php
-    /*Exercício - 1  http://localhost/PHP/PHP/exercicios/ex01.php?qtd=50&ct=5.45
+    /*Exercício - 1  http://localhost/PHP/exercicios/ex01.php?qtd=50&ct=5.45
         Faça o algoritmo que calcule o valor em Reais,
      *  correspondente aos dólares que um turista possui no cofre do hotel. O programa deve
         solicitar os seguintes dados: Quantidade de
