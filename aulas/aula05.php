6ecce8e4d16c744d497d438f91401e185755e007
JonasPinto <jonaspinto.ti@outlook.com>
2022-02-19 01:50:09 -0300

    Update aula05.php

--- aulas/aula05.php
+++ aulas/aula05.php
@@ -7,7 +7,7 @@
     </head>
     <body>
         <div>
-        <?php /*http://localhost/PHP/PHP/aulas/aula05.php?a=5.2&b=8.5&c=12.56*/
+        <?php /*http://localhost/PHP/aulas/aula05.php?a=5&b=8&c=12*/
             $a = $_GET["a"];
             $b = $_GET["b"];
             $c = $_GET["c"];
@@ -16,15 +16,15 @@
             $sub = number_format($soma - $c, 2);            
             $mult = number_format($sub * $a, 2);
             $div = number_format($mult / $b, 2);
-            $mod = number_format($div % 2, 2);
-            $media = number_format(($soma + $sub + $mult + $div + 1000) / 5, 2);
+            $mod = number_format($a % 2, 2);
+            $media = number_format(($soma + $sub + $mult + $div) / 5, 2);
             
         echo "<h3><br/> A soma de $a + $b = $soma 
               <br/> A subtração de $soma - $c = $sub
               <br/> A multiplicação de $sub x $a = $mult
               <br/> A divisão de $mult / $b = $div 
-              <br/> O resto da divisão entre $div e 2 = $mod 
-              <br/> A média entre $soma + $sub + $mult + $div + 1000 = $media <h3/>";
+              <br/> O resto da divisão entre $a e 2 = $mod 
+              <br/> A média entre $soma + $sub + $mult + $div = $media <h3/>";
         ?>
         </div>
     </body>
