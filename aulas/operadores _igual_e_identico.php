2e0c273ec55bae035f1871329c26763b400f01d9
Jonas Silva <jonas.silva@easolucinfo.com>
2022-02-24 18:46:01 -0300

    Create operadores _igual_e_identico.php

---
+++ aulas/operadores _igual_e_identico.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <title>operadores relacionais</title>
+        <link rel="stylesheet" href="../_css/estilo.css"/>
+        
+    </head>
+    <body>
+        <div>
+            <?php
+                $a = 3;
+                $b = "3";
+                
+                # o valor das duas variaveis é 3 mas não são identicas pois em $b o tipo é string
+                echo "Os valores passados foram $a e $b<br/>";
+                
+                $r = $a == $b ? "sim" : "não"; #   
+                echo "As duas variaveis são iguais? $r<br/>";
+                
+                $r = $a === $b ? "sim" : "não";
+                echo "As duas variaveis são identicas? $r<br/>";
+            ?>
+        </div>
+    </body>
+</html>
