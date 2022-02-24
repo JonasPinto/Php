4c59cdf31f9283d1fd0f47adcd53e9d20f341a38
Jonas Silva <jonaspinto.ti@outlook.com>
2022-02-24 01:35:20 -0300

    Create incremento.php

---
+++ aulas/incremento.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Incremento</title>
+        <link rel="stylesheet" href="../_css/estilo.css"/>
+    </head>
+    
+    <body>
+    <div>
+        <?php /*http://localhost/PHP/aulas/incremento.php?idade=34*/
+        /* ++$variável a variável é incrementada primeiro
+           $variável++ 
+           --$variável
+           $variável-- */
+        
+        $atual = $_GET["aa"];
+        
+        echo "O ano atual é $atual e o ano anterior é ".--$atual
+             ."<br/>O próximo ano depois de ".++$atual. " é ".++$atual;      
+        ?>
+    <div/>    
+    </body>
+</html>
