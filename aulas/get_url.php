7b2cd33b27fca31708a1adf2f350df8a1d185c0d
Jonas Silva <jonas.silva@easolucinfo.com>
2022-02-24 18:46:47 -0300

    Create get_url.php

---
+++ aulas/get_url.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Get url</title>
+        <link rel="stylesheet" href="../_css/estilo.css"/>
+    </head>
+    
+    <body>
+    <div>
+        <?php 
+        $idade = $_GET["idade"];
+        $nome = "Jonas";
+        echo " $nome tem $idade anos ";      
+        ?>
+    <div/>    
+    </body>
+</html>
