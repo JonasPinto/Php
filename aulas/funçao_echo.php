2131cd98041c56c7bd5b2c90ef90322916b09c80
Jonas Silva <jonas.silva@easolucinfo.com>
2022-02-24 18:46:43 -0300

    Create funçao_echo.php

---
+++ aulas/funçao_echo.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>função echo</title>
+        <style>
+            h2 {
+                color:#80a2ff;
+                    text-shadow: 1px 1px 1px black;
+            }
+            h3 {
+                tex
+                text-align: center;
+                color:#cc0000;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>Testando PHP</h1>
+        <?php
+        $idade = 3;
+
+echo "<h2>Olá,<br/> Mundo!</h2>";
+        ?>
+    </body>
+</html>
