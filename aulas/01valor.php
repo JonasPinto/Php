4480f08425a5ef08169b43811af02b53245be5f2
Jonas Silva <jonas.silva@easolucinfo.com>
2022-03-02 18:23:07 -0300

    Create 01valor.php

---
+++ aulas/01valor.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <link rel="stylesheet" href="../_css/estilo.css"/>
+        <title>01valor</title>
+    </head>
+    <body>
+        <div>
+            <?php
+                $valor = $_GET["v"];
+                $rq = sqrt($valor);
+                echo "O valor enviado foi $valor<br/>A sua raiz é = ".number_format($rq, 2);              
+            ?>
+            <a href="formularios.php"><botton>Enviar</botton></a>       
+        </div>
+    </body>
+</html>
