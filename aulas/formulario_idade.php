3b43bbf040fbee6f6c6f596a7581136638e770cb
Jonas Silva <jonas.silva@easolucinfo.com>
2022-03-02 18:22:59 -0300

    Create formulario_idade.php

---
+++ aulas/formulario_idade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <link rel="stylesheet" href="../_css/estilo.css"/>
+        <title>formulário 01</title>
+    </head>
+    <body>
+        <div>
+            <form method ="get" action ="02idade.php">
+                Nome: <input type="text" name="nome"/><br/>
+                Ano de Nascimento: <input type="number" name="ano"/><br/>
+                <fieldset><legend>Sexo</legend>
+                    <input type="radio" name="sexo" id="masc" value="Homem" checked/>
+                    <label for="masc">Masculino</label><br/>
+                    <input type="radio" name="sexo" id="fem" value="Mulher"/>
+                    <label for="fem">Feminino</label>
+                </fieldset><br/>
+                <input type="submit" value="Enviar"/>
+            </form>
+        </div>
+    </body>
+</html>
