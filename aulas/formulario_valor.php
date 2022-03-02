2af2bcfa12d1fc2052d7f469cea0c5ec24cdd0c3
Jonas Silva <jonas.silva@easolucinfo.com>
2022-03-02 18:22:57 -0300

    Create formulario_valor.php

---
+++ aulas/formulario_valor.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <link rel="stylesheet" href="../_css/estilo.css"/>
+        <title>formulários</title>
+    </head>
+    <body>
+        <div>
+            <form method ="get" action ="01valor.php">
+                valor: <input type="number" name="v"/>
+                <input type="submit" value="caucular raiz"/>
+            </form>
+        </div>
+    </body>
+</html>
