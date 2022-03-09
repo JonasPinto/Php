656c6e567adee11e0ac827accd541b56d984055d
Jonas Silva <jonas.silva@easolucinfo.com>
2022-03-08 23:35:58 -0300

    Create condicionais_if_else.php

---
+++ aulas/condicionais_if_else.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<<html>
+    <head>
+        <link rel="stylesheet" href="../_css/estilo.css"/>
+        <meta charset="UTF-8">
+        <title>IF and ELSE</title>
+    </head>
+    <body>
+        <div>
+            <form method="get" action="exercicio_if.php">
+                Ano de Nascimento:
+                <input type="number" placeholder="4 digitos" name="ano"/>
+                <input type="submit" value="Verificar"/>
+            </form>
+        </div>
+
+    </body>
+</html>
+
