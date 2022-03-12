3ac159de46c7e88e8f059ad03b4163f908e6e803
Jonas Silva <jonas.silva@easolucinfo.com>
2022-03-12 13:48:26 -0300

    Update media.php

--- aulas/media.php
+++ aulas/media.php
@@ -1,7 +1,19 @@
-<?php
-
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
-
+<!doctype html>
+<html>
+    <head>
+        <link rel="stylesheet" href="../_css/notas.css"/>
+        <meta charset="UTF-8">
+        <title>Cauladora de Média</title>
+    </head>
+    <body>
+        <div class="texto">
+            <form method="get" action="notas.php">
+                <label for="n1">Nota 1: </label>
+                <input type="number" name="n1" id="n1" placeholder="N1" step="0.01"><br/>
+                <label for="n2">Nota 2: </label>
+                <input type="number" name="n2" id="n2" placeholder="N2" step="0.01" ><br/> 
+                <input id="sub" type="submit" value="Caucular Média">
+            </form>
+        </div>
+    </body>
+</html>
