15abcb0ad13af70004b96d28984a865cb4f33d41
Jonas Silva <jonas.silva@easolucinfo.com>
2022-03-14 17:48:13 -0300

    Update media.php

--- aulas/media.php
+++ aulas/media.php
@@ -12,7 +12,7 @@
                 <input type="number" name="n1" id="n1" placeholder="N1" step="0.01"><br/>
                 <label for="n2">Nota 2: </label>
                 <input type="number" name="n2" id="n2" placeholder="N2" step="0.01" ><br/> 
-                <input id="sub" type="submit" value="Caucular Média">
+                <input class="sub" type="submit" value="Caucular Média">
             </form>
         </div>
     </body>
