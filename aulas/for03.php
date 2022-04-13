2f0183f4eb420649d6f3f384300ac3aae8a03f35
Jonas Silva <jonas.silva@easolucinfo.com>
2022-04-13 18:13:48 -0300

    Create for03.php

---
+++ aulas/for03.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+    <head>
+        <title>for 03</title>
+        <link rel="stylesheet" href="../_css/estilo.css">
+    </head>
+    <body>
+        <div>
+            <form action="for_03.php">
+                <label for="idNum">Número</label>
+                <input name="num" id="idNum" type="number" required/>
+                <input type="submit" value="É primo?"/>
+            </form>            
+        </div>
+    </body>
+</html>
