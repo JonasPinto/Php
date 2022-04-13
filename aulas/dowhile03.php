ef00818387085cd20b9f93c8ccfdce927e4958aa
Jonas Silva <jonas.silva@easolucinfo.com>
2022-04-13 18:14:18 -0300

    Create dowhile03.php

---
+++ aulas/dowhile03.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Get url</title>
+        <link rel="stylesheet" href="../_css/estilo.css"/>
+    </head>
+    
+    <body>
+    <div>
+        <form method="get" action="dowhile_03.php">
+            <label for="tab">Tabuada</label>
+            <select type="number" name="tab" required>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+                <option>6</option>
+                <option>7</option>
+                <option>8</option>
+                <option>9</option>
+                <option>10</option>
+            </select>
+            <input type="submit" value="Ir para Tabuada">            
+        </form>
+    </div>    
+    </body>
+</html>
